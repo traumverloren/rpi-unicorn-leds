e4f037085c6ff69e98992b116e99429b560177d7
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-03 17:39:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![under construction gif](https://www.heelzwaarleven.nl/underconstruction.gif)
+
+## rpi-unicorn-leds
+
+A WIP and first attempt at raspberry interactive art.
---
+++ rpi-client/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rpi-unicorn-client',
+    version='0.1',
+    packages=['unicornhat'],
+    url='https://github.com/traumverloren/rpi-unicorn-leds',
+    license='MIT',
+    author='Stephanie Nemeth',
+    author_email='traumverloren@gmail.com',
+    description='Raspberry Pi + Unicorn Hat LED client'
+)
