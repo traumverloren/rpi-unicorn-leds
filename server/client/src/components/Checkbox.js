7c1174e350cd14e4007fcd204a778f4cf9f6bc9c
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-06 22:20:37 +0100

    add react client

---
+++ server/client/src/components/Checkbox.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+const io = require('socket.io-client/socket.io');
+const socket = io('https://peaceful-oasis-97526.herokuapp.com', {});
+
+class Checkbox extends Component {
+  constructor() {
+    super();
+    this.state = {checked: false};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({checked: event.target.checked});
+    // console.log(this.state.checked);
+    socket.emit('stateChanged', "Button Pressed from React Web!");
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="checkbox"
+          checked={this.state.checked}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+}
+
+export default Checkbox;
