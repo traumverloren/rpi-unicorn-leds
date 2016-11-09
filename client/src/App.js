662aaf8bb38ead66a0c1988ddf35c630428365c3
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-09 12:16:57 +0100

    check for client key when react app connects to server

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,6 @@ import React, { Component } from 'react'
 import './App.css'
 import Board from './components/Board'
 
-const io = require('socket.io-client/socket.io');
-const socket = io('https://peaceful-oasis-97526.herokuapp.com', {});
-
-socket.on('connect', () => {
-  console.log('React Web app is connected to the Server!');
-});
-
-socket.on('updateState', function (data) {
-    console.log(data);
-  });
-
 class App extends Component {
   render() {
     return (
