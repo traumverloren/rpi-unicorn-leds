8e86bd2453a391a58a2e372cb87b69b3e921737c
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-11 16:10:41 +0100

    get rid of bootstrap, moar css geolize style

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>Made with <a href="https://traumverloren.github.io"><i className="fa fa-heart-o" style={{color: 'Fuchsia', }} aria-hidden="true"></i></a> by Stephanie </p>
+      </div>
+  );
+}
+export default Footer;
