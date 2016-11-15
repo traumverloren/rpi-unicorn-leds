9ddbfc9b38921045382d3ad7708e6c62f3d588ef
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-15 12:02:33 +0100

    add squares grid

--- react-native-client/main.js
+++ react-native-client/main.js
@@ -8,6 +8,7 @@ import {
   StyleSheet,
   Text,
   View,
+  ListView,
   TouchableHighlight,
 } from 'react-native';
 
@@ -27,15 +28,12 @@ class App extends Component {
       'PressStart2P-Regular': require('./assets/fonts/PressStart2P-Regular.ttf'),
       'VT323-Regular': require('./assets/fonts/VT323-Regular.ttf'),
     });
-
     this.setState({ fontLoaded: true });
   }
 
   constructor () {
     super()
-
     this.state = { piConnected: true, fontLoaded: false }
-
     this.fetchPiStatus()
 
     socket.on('connect', () => {
@@ -73,7 +71,7 @@ class App extends Component {
     return (
       <View style={styles.container}>
         <Header fontLoaded={this.state.fontLoaded} />
-        <Square />
+        <Board />
         <Footer />
       </View>
     );
@@ -81,8 +79,73 @@ class App extends Component {
 }
 
 
+class Board extends Component {
+  constructor(props) {
+    super()
+    this.state = this.getBoard()
+  }
 
-// function Square({ isSelected, onClick, color }) {
+  getBoard = () => {
+    const squares = []
+
+    // i is the square number 0 - 63.
+    var i = 0
+
+    // Loop through to generate the x,y coords.
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        squares.push({
+            id: i,
+            coords: [x,y],
+            isSelected: false,
+            color: {r: 44, g: 62, b: 80}
+        })
+        i++
+      }
+    }
+
+    // set initial state with 64-array of squares
+    return { squares, isSubmitted: false, color: {r: 255, g: 235, b: 59} }
+  }
+
+  clearBoard = (event) => {
+    this.setState(this.getBoard())
+    event.target.blur()
+  }
+
+  submitBoard = (event) => {
+    this.setState({isSubmitted: true})
+    event.target.blur()
+    this.props.sendMessage('stateChanged', {message: "Light Design Submitted", squares: this.state.squares } )
+  }
+
+  handlePress = (id) => {
+    console.log(id)
+    const squares = this.state.squares.slice()
+    squares[id].isSelected = !squares[id].isSelected
+    squares[id].color = this.state.color
+    this.setState({squares: squares})
+  }
+
+  render () {
+    return (
+      <View style={styles.squares}>
+        {this.state.squares.map((square) => (
+          <View key={square.id} style={styles.square}>
+            <Square
+              id={square.id}
+              isSelected={square.isSelected}
+              coords={square.coords}
+              color={square.color}
+              onPress={() => this.handlePress(square.id)} />
+          </View>
+        ))}
+      </View>
+    )
+  }
+}
+
+function Square({ isSelected, onPress, color }) {
   // var squareStyle;
   //
   // if (isSelected) {
@@ -91,42 +154,35 @@ class App extends Component {
   //   squareStyle = {backgroundColor: '#2c3e50', width: '100%', height: '100%'}
   // }
 
-  class Square extends Component {
-    _onPressButton() {
-      console.log("You tapped the button!");
-      socket.emit('stateChanged', "Button Pressed from React Native!");
-    }
+  var squareStyle;
 
-    render() {
-      var squareStyle;
+  // if (isSelected) {
+  //   squareStyle = {backgroundColor: 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')', width: '100%', height: '100%'}
+  // } else {
+    squareStyle = {backgroundColor: '#2c3e50', width: 38, height: 38}
+  // }
+  return (
+    <View>
+      <TouchableHighlight onPress={onPress} style={squareStyle}>
+        <View />
+      </TouchableHighlight>
+    </View>
 
-      // if (isSelected) {
-      //   squareStyle = {backgroundColor: 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')', width: '100%', height: '100%'}
-      // } else {
-        squareStyle = {backgroundColor: '#2c3e50', width: '100%', height: '100%'}
-      // }
-      return (
-        <View style={{flex: 3}}>
-          <TouchableHighlight onPress={this._onPressButton} style={squareStyle}>
-            <View />
-          </TouchableHighlight>
-        </View>
-
-      );
-    }
-
-  }
+  );
+}
 
 function Header({ fontLoaded }) {
   return (
-    <View style={{flex: 1, flexDirection: 'column'}}>
+    <View style={styles.header}>
       {
         fontLoaded ? (
         <View style={{flex: 1, alignItems: 'center'}}>
           <Text style={{ ...Font.style('PressStart2P-Regular'), fontSize: 16, marginTop: 20 }}>
             Make Pixel LED Art
           </Text>
-          <Text style={{ ...Font.style('VT323-Regular'), fontSize: 16, marginTop: 10, marginLeft: 30, marginRight: 30, textAlign: 'center' }}><FontAwesome name="magic" size={14} /> Pick colors, click squares, & send a design to my Raspberry Pi!</Text>
+          <Text style={{ ...Font.style('VT323-Regular'), fontSize: 18, marginTop: 10, marginBottom: 4,marginLeft: 40, marginRight: 40, textAlign: 'center', color: 'blue' }}><FontAwesome name="magic" size={14} /> Pick colors.</Text>
+          <Text style={{ ...Font.style('VT323-Regular'), fontSize: 18, marginBottom: 4, marginLeft: 40, marginRight: 40, textAlign: 'center', color: 'teal' }}><FontAwesome name="hand-pointer-o" size={14} /> Click squares.</Text>
+          <Text style={{ ...Font.style('VT323-Regular'), fontSize: 18, marginLeft: 40, marginBottom: 4, marginRight: 40, textAlign: 'center', color: 'green'}}><FontAwesome name="envelope-o" size={14} /> Send a design to my Raspberry Pi!</Text>
         </View>
         ) : null
       }
@@ -136,12 +192,11 @@ function Header({ fontLoaded }) {
 
 function Footer() {
   return (
-      <View style={{height: 30}}>
+      <View style={styles.footer}>
         <Text>Made with <Entypo name="heart-outlined" size={20} color="red" /> by Stephanie </Text>
       </View>
     );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -151,6 +206,30 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: 'column',
   },
+  header: {
+    flex: .2,
+    marginTop: 20,
+  },
+  footer: {
+    flex: .1,
+    marginBottom: 5,
+  },
+  square: {
+    width: 38,
+    height: 38,
+    margin: -1,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'black'
+  },
+  squares: {
+    flex: .7,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300
+  }
 });
 
 Exponent.registerRootComponent(App);
