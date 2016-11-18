9594c2c94a4cf4ce60b368f89005c1943e46c07f
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-18 19:11:50 +0100

    refactor header & footer out into own component, tweaker with css

--- server/client/src/components/Board.js
+++ server/client/src/components/Board.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Square from './Square'
 import { HuePicker } from 'react-color'
-import Footer from './Footer'
 
 class Board extends Component {
   constructor(props) {
@@ -68,8 +67,6 @@ class Board extends Component {
     return (
       <div>
         {alert}
-        <h4>Make Pixel LED Art</h4>
-        <p><i className="fa fa-magic"></i> Pick colors, click squares, & send a design to my Raspberry Pi!</p>
         <div style={{display: 'flex', justifyContent: 'center', margin: '20px 0'}}>
           <div style={{width: '316px'}}>
             <HuePicker
@@ -115,7 +112,6 @@ class Board extends Component {
             Clear
           </button>
         </div>
-        <Footer />
       </div>
 
     )
