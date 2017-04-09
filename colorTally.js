0fb59b5924fe65f24d0639f061cb6c8fb0d20a5a
Stephanie Nemeth <traumverloren@gmail.com>
2017-04-09 18:48:53 +0200

    add new module to find top 2 colors in design and emit to shirt

---
+++ colorTally.js
@@ -0,0 +1,33 @@
+var convert = require('color-convert');
+
+const findTopColors = (state) => {
+  console.log("finding top colors! ✨");
+
+  // go through squares array, find colors, convert to HEX
+  const colorsUsed = state.squares.filter((square) => (square.isSelected))
+                            .map(square => square.color)
+                            .map(square => convert.rgb.hex(square.r, square.g, square.b));
+
+  // tally up the occurrences of colors
+  const countedColors = colorsUsed.reduce((allColors, color) => {
+    if (color in allColors) {
+      allColors[color]++;
+    } else {
+      allColors[color] = 1;
+    }
+    return allColors;
+  }, {});
+
+ // sort the array by the top color
+ const sortedColors = Object.keys(countedColors)
+                            .sort(function(a,b){return countedColors[b]-countedColors[a]})
+
+  // return the top 2 colors to the shirt
+  if (sortedColors.length === 1) {
+    return [sortedColors[0], sortedColors[0]];
+  } else {
+    return [sortedColors[0], sortedColors[1]];
+  }
+}
+
+module.exports = findTopColors;
